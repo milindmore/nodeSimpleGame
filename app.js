27c4f204701b071d4ebe7ff907b4349a121cc0f1
Nicolas Garaza <nicolas.garaza@gmail.com>
2013-01-11 19:52:57 -0300

    First version of the app
    This is in developing stage. This is not a stable version

---
+++ app.js
@@ -0,0 +1,42 @@
+
+/**
+ * Module dependencies.
+ */
+
+var express = require('express'), routes = require('./routes'), user = require('./routes/user'),
+    http = require('http'), path = require('path'),
+    util = require('util');
+
+var app = express();
+
+app.configure(function(){
+  app.set('port', process.env.PORT || 3000);
+  app.set('views', __dirname + '/views');
+  app.set('view engine', 'ejs');
+  app.use(express.favicon());
+  app.use(express.logger('dev'));
+  app.use(express.bodyParser());
+  app.use(express.methodOverride());
+  app.use(express.cookieParser('your secret here'));
+  app.use(express.session());
+  app.use(app.router);
+  app.use(require('stylus').middleware(__dirname + '/public'));
+  app.use(express.static(path.join(__dirname, 'public')));
+});
+
+app.configure('development', function(){
+  app.use(express.errorHandler());
+});
+
+app.get('/', routes.index);
+app.get('/users', user.list);
+
+var server = http.createServer(app);
+var io = require('socket.io').listen(server);
+
+
+server.listen(app.get('port'), function(){
+  console.log("Express server listening on port " + app.get('port'));
+});
+
+io.sockets.on('connection', function (socket) {});
